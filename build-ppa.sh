46164de98d7542308745e9045e59ee383764bedd
RincewindWizzard <git@magierdinge.de>
2023-04-10 13:34:10 +0200

    build

--- build-ppa.sh
+++ build-ppa.sh
@@ -1,5 +1,8 @@
-PAGES_PATH="target/html/"
+PAGES_PATH="target/gh-pages"
+DEBIAN_PACKAGE_PATH="target/debian"
 
-mkdir $PAGES_PATH
+mkdir $PAGES_PATH 2>/dev/null
 
-cp target/release/papierkram $PAGES_PATH
+cp ${DEBIAN_PACKAGE_PATH}/*.deb $PAGES_PATH/
+
+dpkg-scanpackages --multiversion $PAGES_PATH | gzip -9c >$PAGES_PATH/Packages.gz
